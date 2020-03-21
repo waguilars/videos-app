8852d5666d3d392f86b44b5134a62aee25942c04
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-03-21 12:34:28 -0500

    configuracion de cors

--- api-symfony/public/index.php
+++ api-symfony/public/index.php
@@ -4,20 +4,34 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+header('Access-Control-Allow-Origin: *');
+header(
+	'Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization'
+);
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+header('Allow: GET, POST, OPTIONS, PUT, DELETE');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == 'OPTIONS') {
+	die();
+}
+
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+	umask(0000);
 
-    Debug::enable();
+	Debug::enable();
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+	Request::setTrustedProxies(
+		explode(',', $trustedProxies),
+		Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
+	);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
+	Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
